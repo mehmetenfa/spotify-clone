3c2cfa42de794040705ca261ea189d5d7c026803
Mehmet Enfa Vakitsayan <mehmet.enfa@gmail.com>
2022-10-15 20:38:57 +0300

    left nac and logo

---
+++ src/components/Sidebar/Menu.js
@@ -0,0 +1,27 @@
+
+
+const Menu = () => {
+  return (
+    <nav className="px-2">
+        <ul className="flex flex-col">
+            <li>
+                <a href="#" className="h-10 flex items-center text-sm font-semibold">
+                    Anasayfa
+                </a>
+            </li>
+            <li>
+                <a href="#" className="h-10 flex items-center text-sm font-semibold">
+                    Ara
+                </a>
+            </li>
+            <li>
+                <a href="#" className="h-10 flex items-center text-sm font-semibold">
+                    Kitaplıgın
+                </a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Menu
