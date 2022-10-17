5c263820c7a2ea383997f70fcfa60896a7037d21
Mehmet Enfa Vakitsayan <mehmet.enfa@gmail.com>
2022-10-17 18:40:42 +0300

    icon start

--- src/components/Sidebar/Menu.js
+++ src/components/Sidebar/Menu.js
@@ -5,17 +5,17 @@ const Menu = () => {
     <nav className="px-2">
         <ul className="flex flex-col">
             <li>
-                <a href="#" className="h-10 flex items-center text-sm font-semibold">
+                <a href="#" className="h-10 flex items-center text-sm font-semibold text-white rounded hover:text-white px-4 bg-active ">
                     Anasayfa
                 </a>
             </li>
             <li>
-                <a href="#" className="h-10 flex items-center text-sm font-semibold">
+                <a href="#" className="h-10 flex items-center text-sm font-semibold text-link rounded hover:text-white px-4 ">
                     Ara
                 </a>
             </li>
             <li>
-                <a href="#" className="h-10 flex items-center text-sm font-semibold">
+                <a href="#" className="h-10 flex items-center text-sm font-semibold text-link rounded hover:text-white px-4">
                     Kitaplıgın
                 </a>
             </li>
