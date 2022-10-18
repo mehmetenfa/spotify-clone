526580f0eb4b33446f21e69493c6f54aef0ce0f4
Mehmet Enfa Vakitsayan <mehmet.enfa@gmail.com>
2022-10-18 07:30:16 +0300

    icons file

--- src/components/Sidebar/Menu.js
+++ src/components/Sidebar/Menu.js
@@ -1,11 +1,14 @@
-
+import { Icon } from 'Icons';
 
 const Menu = () => {
   return (
     <nav className="px-2">
         <ul className="flex flex-col">
             <li>
-                <a href="#" className="h-10 flex items-center text-sm font-semibold text-white rounded hover:text-white px-4 bg-active ">
+                <a href="#" className="h-10 flex items-center text-sm font-semibold text-white  rounded hover:text-white px-4 bg-active ">
+                    <span>
+                        <Icon name="home" />
+                    </span>
                     Anasayfa
                 </a>
             </li>
